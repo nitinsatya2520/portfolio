881ff823356f7863975409008870e958d83ca8be
nitinsatya2520 <nitinsatya6561@gmail.com>
2025-06-29 18:00:39 +0530

    Update Preloader.js

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,11 +1,24 @@
 // src/components/Preloader.js
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import './Preloader.css';
 
+const tags = ["Nitin","Satya", "Developer", "Designer", "Creator", "Engineer","Techverra"];
+
 const Preloader = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const tagInterval = setInterval(() => {
+      setIndex(prev => (prev + 1) % tags.length);
+    }, 400); // Change every 0.4s
+
+    return () => clearInterval(tagInterval);
+  }, []);
+
   return (
-    <div className="preloader">
-      <div className="spinner"></div>
+    <div className="preloader-container">
+      <div className="zoom-text">{tags[index]}</div>
     </div>
   );
 };
